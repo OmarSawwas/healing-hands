2134a69b6a9a7a4977fd39c51b10c01a8adbea43
Omar Sawwas <sawwas.omar@gmail.com>
2022-03-30 13:12:21 +0300

    Change9 after hosting

--- src/Pages/Animal-Care/SingleCard.jsx
+++ src/Pages/Animal-Care/SingleCard.jsx
@@ -96,18 +96,18 @@ const AnimalCard = (props) => {
 		// Doc Reference
 
 		// Change name submit button
-		const submitPercentage = async () => {
-			if (percentage > 0 && percentage < 100) {
-				const upDate = async () => {
-					if (Id) {
-						await updateDoc(docRef, {percentageCompleted: percentage});
-					}
-				};
+	};
+	const submitPercentage = async () => {
+		if (percentage > 0 && percentage < 100) {
+			const upDate = async () => {
+				if (Id) {
+					await updateDoc(docRef, {percentageCompleted: percentage});
+				}
+			};
 
-				upDate();
-			}
-			setEditMode(false);
-		};
+			upDate();
+		}
+		setEditMode(false);
 	};
 	const deletePost = async () => {
 		setPropOpen(true);
